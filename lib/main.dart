7c3ad25148df0690a795c0b08e8493fb6118bfce
Eagl3ye <vincejaphethloable@gmail.com>
2023-04-19 19:35:10 +0800

    build: Add Firebase Core initializations in the main dart file

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
 import 'package:attendance/pages/qrview.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
