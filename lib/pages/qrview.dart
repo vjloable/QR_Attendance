44d3c95af375ec5fcef52b70758bbcb36636549e
Eagl3ye <vincejaphethloable@gmail.com>
2023-05-06 08:46:39 +0800

    feat: Adds SHOW DETAILS button in the center of the scan overlay

--- lib/pages/qrview.dart
+++ lib/pages/qrview.dart
@@ -13,6 +13,7 @@ class _QRViewPageState extends State<QRViewPage> {
   final GlobalKey qrKey = GlobalKey(debugLabel: 'QR');
   Barcode? result;
   QRViewController? controller;
+  Color isScanned = Colors.deepOrange;
 
   @override
   void reassemble() {
@@ -38,33 +39,54 @@ class _QRViewPageState extends State<QRViewPage> {
                 Container(
                   height: MediaQuery.of(context).size.height * 0.1,
                   width: double.infinity,
-                  color: Colors.deepPurple,
+                  color: Colors.deepOrange,
                 ),
                 SizedBox(
                   height: MediaQuery.of(context).size.height  * 0.9,
                   width: double.infinity,
-                  child: QRView(
-                    overlay: QrScannerOverlayShape(
-                        borderWidth: 20,
-                        borderRadius: 20,
-                        borderColor: Colors.deepPurple,
-                        cutOutBottomOffset: 100,
-                    ),
-                    key: qrKey,
-                    onQRViewCreated: _onQRViewCreated,
+                  child: Stack(
+                    alignment: AlignmentDirectional.center,
+                    children: [
+                      QRView(
+                        overlay: QrScannerOverlayShape(
+                            borderWidth: 20,
+                            borderRadius: 20,
+                            borderColor: isScanned,
+                        ),
+                        key: qrKey,
+                        onQRViewCreated: _onQRViewCreated,
+                      ),
+                      Positioned(
+                        bottom: MediaQuery.of(context).size.height  * 0.2,
+                        child: Center(
+                          child: (result != null)
+                              ? Text('${result!.code}', style: const TextStyle(color: Colors.white))
+                              : const Text('Scan a code', style: TextStyle(color: Colors.white)),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 150,
+                        width: 150,
+                        child: Opacity(
+                          opacity: 0.7,
+                          child: ElevatedButton(
+                            onPressed: () {},
+                            style: ElevatedButton.styleFrom(
+                                foregroundColor: Colors.black,
+                                backgroundColor: Colors.deepOrange,
+                                shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
+                            ),
+                            child: const Text(
+                              'SHOW DETAILS',
+                              style: TextStyle(fontSize: 18), textAlign: TextAlign.center,
+                            ),
+                          ),
+                        ),
+                      )
+                    ],
                   )
                 ),
               ],
-            ),
-          ),
-          Container(
-            height: MediaQuery.of(context).size.height  * 1,
-            width: double.infinity,
-            color: Colors.transparent,
-            child: Center(
-              child: (result != null)
-                  ? Text('${result!.code}', style: const TextStyle(color: Colors.white))
-                  : const Text('Scan a code', style: TextStyle(color: Colors.white)),
             ),
           ),
         ],
@@ -78,9 +100,9 @@ class _QRViewPageState extends State<QRViewPage> {
       if(scanData.code!.length >= 9){
         await controller.pauseCamera();
       }
-
       setState(() {
         result = scanData;
+        isScanned = Colors.lightGreenAccent;
       });
     });
   }
