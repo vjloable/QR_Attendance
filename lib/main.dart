205a27355727bd10a8649ab5067ef34d8e592a54
Eagl3ye <vincejaphethloable@gmail.com>
2023-06-12 02:03:23 +0800

    refactor: transfer permission handling for sms during main initialization

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,10 @@ import 'firebase_options.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Permission.camera.request();
+  await [
+    Permission.sms,
+    Permission.camera,
+  ].request();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
