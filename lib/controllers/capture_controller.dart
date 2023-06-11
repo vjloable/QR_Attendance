861aebfaa763ff76b02e9d923ed41f861153e509
Eagl3ye <vincejaphethloable@gmail.com>
2023-06-12 02:07:15 +0800

    feat: add error enum type, and setters/getters that returns status color, status icon data, and status

--- lib/controllers/capture_controller.dart
+++ lib/controllers/capture_controller.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:mobile_scanner/mobile_scanner.dart';
 
 class CaptureController extends ChangeNotifier {
   CaptureState state = CaptureState.scanning;
   List<Barcode> _barcodes = [];
+  StudentState status = StudentState.unmarked;
 
   void flagCaptured({required List<Barcode> barcodes}) {
     state = CaptureState.captured;
@@ -21,6 +23,11 @@ class CaptureController extends ChangeNotifier {
     notifyListeners();
   }
 
+  void flagError() {
+    state = CaptureState.error;
+    notifyListeners();
+  }
+
   List<Barcode> getBarcodes() {
     return _barcodes;
   }
@@ -29,10 +36,60 @@ class CaptureController extends ChangeNotifier {
     return _barcodes.length;
   }
 
+  void setStatus(bool state) {
+    switch(state) {
+      case true:
+        status = StudentState.onTime;
+        break;
+      case false:
+        status = StudentState.late;
+        break;
+    }
+  }
+
+  String getStatus() {
+    switch(status) {
+      case StudentState.late:
+        return "LATE";
+      case StudentState.onTime:
+        return "ON-TIME";
+      case StudentState.unmarked:
+        return "UNMARKED";
+    }
+  }
+
+  Color getStatusColor() {
+    switch(status) {
+      case StudentState.late:
+        return Colors.red;
+      case StudentState.onTime:
+        return Colors.green;
+      case StudentState.unmarked:
+        return Colors.grey;
+    }
+  }
+
+  IconData getStatusIconData() {
+    switch(status) {
+      case StudentState.late:
+        return Icons.timer_off_outlined;
+      case StudentState.onTime:
+        return Icons.timer;
+      case StudentState.unmarked:
+        return Icons.hourglass_empty;
+    }
+  }
 }
 
 enum CaptureState {
   scanning,
   scanned,
   captured,
+  error,
+}
+
+enum StudentState {
+  late,
+  onTime,
+  unmarked,
 }
