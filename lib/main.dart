1206b2602b80f3b93a7d1e35e77c9d8400c1ac5d
Eagl3ye <vincejaphethloable@gmail.com>
2023-06-11 15:52:17 +0800

    fix: add camera permission request on start

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,13 @@
-import 'package:attendance/pages/qrview.dart';
+import 'package:attendance/views/qrview.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:permission_handler/permission_handler.dart';
 import 'firebase_options.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Permission.camera.request();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
@@ -29,7 +31,7 @@ class MyApp extends StatelessWidget {
       title: 'Attendance',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primarySwatch: Colors.deepPurple,
+        primarySwatch: Colors.deepOrange,
       ),
       home: const QRViewPage(),
     );
